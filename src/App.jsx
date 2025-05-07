70ff796b1868bb753ec78d96e634e5a5f029e262
Mayron Castillo <12mayron12@gmail.com>
2025-05-07 17:00:12 -0600

    Mejora en el agregar al carrito en el diseño y agregué el componente de Cart que es donde se van a mostrar los productos que se añaden

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import './App.css'
+import Cart from './components/Cart'
 import Datos from './components/Datos'
 
 
 function App() {
   return (
     <>
-      <section className='w-full grid grid-cols-[4fr_2fr]'>
-        <section className='bg-gray-300 flex flex-col pl-32'>
+      <section class='bg-gray-300 w-full grid grid-cols-[4fr_2fr] pt-8'>
+        <section class=' flex flex-col pl-32'>
           <div> 
-            <h1 className='font-bold text-4xl pb-8 pt-4'>Desserts</h1>
+            <h1 class='font-bold text-4xl pb-8'>Desserts</h1>
           </div>
             <Datos></Datos>
         </section>
-        <aside className=' bg-red-500'>
-          <p>Hola</p>
+        <aside>
+          <Cart></Cart>
         </aside>
       </section>
     </>
--- src/components/Datos.jsx
+++ src/components/Datos.jsx
@@ -3,18 +3,27 @@ import data from '../data/data.json'
 function Datos() {
     return (
         <section>
-        <div className='grid grid-cols-3 gap-4'>
+        <div className='grid grid-cols-3 gap-2'>
             {data.map((item,index)=>{
                 return(
-                <div key={index} className='flex flex-col gap-2'>
-                    <img src={item.image.desktop} alt="" className='w-[250px] rounded-lg' />
-
-                    <div className='pt-3'>
-                        <p className='text-gray-500 text-sm'>{item.category}</p>
-                        <h2 className='font-bold'>{item.name}</h2>
-                        <p className='text-orange-600 font-bold'>${item.price}</p>
+                <div key={index} className='flex flex-col gap-2 items-start'>
+                    <div className="relative flex flex-col items-center">
+                        <img src={item.image.desktop} alt="" className="w-[280px] rounded-lg" />
+                        
+                        <div className="bg-yellow-50 w-[180px] rounded-full flex justify-center p-3 border border-amber-600 -mt-6">
+                            <button class='flex gap-2 cursor-pointer'>
+                                <img src="/public/assets/images/icon-add-to-cart.svg" alt="" />
+                                <p>Add To Cart</p>
+                            </button>
+                        </div>
                     </div>
-                        <button className='bg-amber-300 w-[250px] rounded-lg cursor-pointer'> 🛒 Add To Cart </button>
+
+                    <div className='pb-6 pt-2'>
+                        <p className='text-gray-500 text-sm'>{item.category}</p>
+                        <h2 className='font-bold text-lg'>{item.name}</h2>
+                        <p className='text-orange-600 font-bold text-lg'>${item.price}</p>
+                    </div>
+                        
                 </div>
                 )
             })}
